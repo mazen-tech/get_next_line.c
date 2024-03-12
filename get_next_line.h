57fd6386f7d51a1b4f6d5dd2e54b1ebc44ce72f5
Mazen Tarek <104716013+mazen-tech@users.noreply.github.com>
2024-03-12 12:54:51 +0100

    Add files via upload

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+#ifndef GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
+
+#include <stdlib.h>
+#include <unistd.h>
+
+//by defining the buffer size to 42 that means the buffer read 42 chars (bytes) per time
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 42 
+#endif
+
+char *get_next_line(int fd);
+
+//get_next_line_utils
+void ft_bzero(void *s, size_t n);
+size_t ft_strlen(const char *str);
+char *ft_strchr(const char *str, int c);
+char *ft_strjoin(const char *s1, const char *s2);
+
+
+#endif
